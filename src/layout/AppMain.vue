8cc7d5eb4d73844d4cb935a8ffabdb372d06f03d
taojun-Rainbox <taojun1994@gmail.com>
2020-09-15 15:49:16 +0800

    新增app-main

---
+++ src/layout/AppMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-main>
+    <v-container fluid>
+      <!-- If using vue-router -->
+      <router-view />
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'AppMain'
+}
+</script>
+
+<style>
+
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,18 +2,21 @@
   <v-app id="inspire">
     <sidebar />
     <navbar />
+    <app-main />
   </v-app>
 </template>
 
 <script>
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
+import AppMain from './AppMain'
 
 export default {
   name: 'Layout',
   components: {
     Sidebar,
-    Navbar
+    Navbar,
+    AppMain
   }
 }
 </script>
