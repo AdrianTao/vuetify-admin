171451aff7e3dad20650586ea8a802bef04f028b
taojun-Rainbox <taojun1994@gmail.com>
2020-09-18 13:50:45 +0800

    解决tagsview路由跳转报错

--- src/layout/Navbar/index.vue
+++ src/layout/Navbar/index.vue
@@ -5,21 +5,34 @@
     height="56"
   >
     <v-app-bar-nav-icon @click.stop="toggleSideBar" />
-    <v-toolbar-title>Vuetify Admin</v-toolbar-title>
+    <v-toolbar-title v-if="showLogo">Vuetify Admin</v-toolbar-title>
+
+    <template v-if="showTagsView" v-slot:extension>
+      <tags-view />
+    </template>
   </v-app-bar>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import TagsView from '../TagsView'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'Navbar',
+  components: {
+    TagsView
+  },
   data() {
     return {
       drawer: null
     }
   },
   computed: {
+    ...mapState({
+      showLogo: state => state.settings.sidebarLogo, // 是否展示logo
+      showTagsView: state => state.settings.tagsView // 是否开启标签栏
+    }),
+
     ...mapGetters([
       'sidebar',
       'avatar',
---
+++ src/layout/TagsView.vue
@@ -0,0 +1,239 @@
+<template>
+  <v-tabs align-with-title>
+    <v-tab
+      v-for="tag in visitedViews"
+      :key="tag.path"
+      class="tag"
+      router
+      :to="{ path: tag.path, query: tag.query, fullPath: tag.fullPath }"
+      @click.middle.native="!isAffix(tag)?closeSelectedTag(tag):''"
+      @contextmenu.prevent.native="openMenu(tag,$event)"
+    >
+      <span class="text">{{ tag.title }}</span>
+      <span
+        v-if="!isAffix(tag)"
+        class="close"
+        @click.prevent.stop="closeSelectedTag(tag)"
+      >
+        <svg-icon icon-class="close" />
+      </span>
+    </v-tab>
+
+    <ul
+      v-show="visible"
+      :style="{left:left+'px',top:top+'px'}"
+      class="contextmenu"
+    >
+      <li @click="refreshSelectedTag(selectedTag)">刷新</li>
+      <li
+        v-if="!isAffix(selectedTag)"
+        @click="closeSelectedTag(selectedTag)"
+      >
+        关闭标签
+      </li>
+      <li @click="closeOthersTags">关闭其他标签</li>
+      <li @click="closeAllTags(selectedTag)">关闭所有标签</li>
+    </ul>
+  </v-tabs>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import path from 'path'
+
+export default {
+  name: 'TagsView',
+  data() {
+    return {
+      visible: false,
+      top: 0,
+      left: 0,
+      selectedTag: {},
+      affixTags: []
+    }
+  },
+  computed: {
+    ...mapState({
+      visitedViews: state => state.tagsView.visitedViews,
+      routes: state => state.permission.routes
+    })
+  },
+  watch: {
+    $route() {
+      this.addTags()
+    },
+
+    visible(value) {
+      if (value) {
+        document.body.addEventListener('click', this.closeMenu)
+      } else {
+        document.body.removeEventListener('click', this.closeMenu)
+      }
+    }
+  },
+  mounted() {
+    this.initTags()
+    this.addTags()
+  },
+  methods: {
+    isActive(route) {
+      return route.path === this.$route.path
+    },
+    isAffix(tag) {
+      return tag.meta && tag.meta.affix
+    },
+    filterAffixTags(routes, basePath = '/') {
+      let tags = []
+      routes.forEach(route => {
+        if (route.meta && route.meta.affix) {
+          const tagPath = path.resolve(basePath, route.path)
+          tags.push({
+            fullPath: tagPath,
+            path: tagPath,
+            name: route.name,
+            meta: { ...route.meta }
+          })
+        }
+        if (route.children) {
+          const tempTags = this.filterAffixTags(route.children, route.path)
+          if (tempTags.length >= 1) {
+            tags = [...tags, ...tempTags]
+          }
+        }
+      })
+      return tags
+    },
+    initTags() {
+      const affixTags = this.affixTags = this.filterAffixTags(this.routes)
+      for (const tag of affixTags) {
+        // Must have tag name
+        if (tag.name) {
+          this.$store.dispatch('tagsView/addVisitedView', tag)
+        }
+      }
+    },
+    addTags() {
+      const { name } = this.$route
+      if (name) {
+        this.$store.dispatch('tagsView/addView', this.$route)
+      }
+      return false
+    },
+
+    refreshSelectedTag(view) {
+      this.$store.dispatch('tagsView/delCachedView', view).then(() => {
+        const { fullPath } = view
+        this.$nextTick(() => {
+          this.$router.replace({
+            path: '/redirect' + fullPath
+          })
+        })
+      })
+    },
+    closeSelectedTag(view) {
+      this.$store.dispatch('tagsView/delView', view).then(({ visitedViews }) => {
+        if (this.isActive(view)) {
+          this.toLastView(visitedViews, view)
+        }
+      })
+    },
+    closeOthersTags() {
+      this.$router.push(this.selectedTag)
+      this.$store.dispatch('tagsView/delOthersViews', this.selectedTag).then(() => {
+        this.moveToCurrentTag()
+      })
+    },
+    closeAllTags(view) {
+      this.$store.dispatch('tagsView/delAllViews').then(({ visitedViews }) => {
+        if (this.affixTags.some(tag => tag.path === view.path)) {
+          return
+        }
+        this.toLastView(visitedViews, view)
+      })
+    },
+    toLastView(visitedViews, view) {
+      const latestView = visitedViews.slice(-1)[0]
+      if (latestView) {
+        this.$router.push(latestView.fullPath)
+      } else {
+        // now the default is to redirect to the home page if there is no tags-view,
+        // you can adjust it according to your needs.
+        if (view.name === 'Dashboard') {
+          // to reload home page
+          this.$router.replace({ path: '/redirect' + view.fullPath })
+        } else {
+          this.$router.push('/')
+        }
+      }
+    },
+    openMenu(tag, e) {
+      const menuMinWidth = 105
+      const offsetLeft = this.$el.getBoundingClientRect().left // container margin left
+      const offsetWidth = this.$el.offsetWidth // container width
+      const maxLeft = offsetWidth - menuMinWidth // left boundary
+      const left = e.clientX - offsetLeft + 15 // 15: margin right
+
+      if (left > maxLeft) {
+        this.left = maxLeft
+      } else {
+        this.left = left
+      }
+
+      this.top = e.clientY
+      this.visible = true
+      this.selectedTag = tag
+    },
+    closeMenu() {
+      this.visible = false
+    },
+    handleScroll() {
+      this.closeMenu()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tag {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .close {
+    margin-left: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 19px;
+    height: 19px;
+    border-radius: 50%;
+
+    &:hover {
+      color: #fff;
+      background: #ccc;
+    }
+  }
+}
+
+.contextmenu {
+  margin: 0;
+  background: #fff;
+  z-index: 3000;
+  position: absolute;
+  list-style-type: none;
+  padding: 5px 0;
+  border-radius: 4px;
+  font-size: 12px;
+  font-weight: 400;
+  color: #333;
+  box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, .3);
+  li {
+    margin: 0;
+    padding: 7px 16px;
+    cursor: pointer;
+    &:hover {
+      background: #eee;
+    }
+  }
+}
+</style>
